2ff32e466d9ce9cf842f8ffa09cbd822574e43a8
PiotrKotowski01 <92581672+PiotrKotowski01@users.noreply.github.com>
2021-10-18 16:49:07 +0200

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,139 @@
+﻿
+
+ //zad4
+
+//
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//
+//int main() {
+//	float a, b, c, d;
+//	float wynik;
+//	cout << "podaj a: " << endl;
+//	cin >> a;
+//	cout << "podaj b: " << endl;
+//	cin >> b;
+//	cout << "podaj c: " << endl;
+//	cin >> c;
+//	cout << "podaj d: " << endl;
+//	cin >> d;
+//	wynik = (a + b + c + d) / 4;
+//	cout << "srednia arytmetyczna liczb wynosi: " << wynik << endl;
+//	return 0;
+//}
+	
+
+//  zad3
+
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//
+//int main()
+//{
+//
+//	float a, b;
+//	cout << "podaj a: ", cin >> a;
+//	cout << "podaj b: ", cin >> b;
+//
+//	if (a == b) {
+//		cout << "liczby sa rowne \n";
+//	}
+//	else if (a > b)
+//		cout << "a jest wieksze" << endl;
+//	else
+//		cout << "b jest wieksze\n";
+//}
+	
+  //zad1
+
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//
+//	int main()
+//	{
+//		int nr_albumu = 65545;
+//		string nr_albumu1 = "w65545";
+//		cout << "nr albumu " << nr_albumu << endl;
+//		cout << "nr albumu " << nr_albumu1 << endl;
+//	}
+	
+
+//zad2
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//
+//
+//	int a, b;
+//	cout << "podaj a: ", cin >> a;
+//
+//	if (a % 2 == 0) {
+//		cout << "liczba parzysta\n";
+//		cout << "kolejna linia tesktu\n";
+//	}
+//	else {
+//		cout << "liczba nie parzysta" << endl;
+//
+//	}
+//
+//}
+
+
+//zad5
+
+
+//#include <iostream>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b;
+//	int wybor;
+//	cout << "podaj pierwsze liczbe: ";
+//	cin >> a;
+//	cout << "podaj druga liczbe: ";
+//	cin >> b;
+//	cout << "wybierz dzialanie: " << endl;
+//	cout << "1.dodawanie" << endl;
+//	cout << "2.odejmowanie" << endl;
+//	cout << "3.mnozenie" << endl;
+//	cout << "4.dzielenie" << endl;
+//	cin >> wybor;
+//
+//	switch (wybor)
+//	{
+//	case 1:
+//		cout << "wynik wynosi: " << a + b << endl;
+//		break;
+//
+//	case 2:
+//		cout << "wynik wynosi: " << a - b << endl;
+//		break;
+//
+//	case 3:
+//		cout << "wynik wynosi: " << a * b << endl;
+//		break;
+//
+//	case 4:
+//		cout << "wynik wynosi: " << a / b << endl;
+//		break;
+//
+//	default:
+//		cout << "nie ma takiej opcji!" << endl;
+//		break;
+//	}
+//	return 0;
+//}
